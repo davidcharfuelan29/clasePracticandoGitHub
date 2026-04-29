0fdfeb352a4d08562ac20b547e9060445114f443
Carlos David <david.charfuelan@utp.edu.co>
2026-04-28 19:22:27 -0500

    Agrege el workflow CI

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from google import genai
 
 
 def format_role(role: str) -> str:
-    return "Tú" if role == "user" else "Chatbot"
+    return "USTED" if role == "user" else "Chatbot"
 
 
 def hash_password(password: str) -> str:
