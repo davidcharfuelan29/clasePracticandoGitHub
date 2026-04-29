680f004eb111c111c07f6a455c41bc3a80f4845d
Carlos David <david.charfuelan@utp.edu.co>
2026-04-28 19:58:06 -0500

    corregi tilde en Tu

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from google import genai
 
 
 def format_role(role: str) -> str:
-    return "usted" if role == "user" else "Chatbot"
+    return "Tú" if role == "user" else "Chatbot"
 
 
 def hash_password(password: str) -> str:
